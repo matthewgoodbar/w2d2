d189f1e6deb5d17f7c58771f1cfcf6fb03e1ba3b
matthew <matthewgoodbar@gmail.com>
2022-10-10 16:18:25 -0700

    Implemented hotel_project

--- hotel_project/lib/hotel.rb
+++ hotel_project/lib/hotel.rb
@@ -1,5 +1,45 @@
 require_relative "room"
 
 class Hotel
+
+  attr_reader :rooms
   
+  def initialize(name, hsh)
+    @name = name
+    @rooms = Hash.new
+    hsh.each {|room_name,cap| @rooms[room_name] = Room.new(cap)}
+  end
+
+  def name
+    words = @name.split(" ")
+    words.map! {|word| word.capitalize}
+    return words.join(" ")
+  end
+
+  def room_exists?(room_name)
+    return @rooms[room_name] != nil
+  end
+
+  def check_in(person, room_name)
+    if room_exists?(room_name)
+        if @rooms[room_name].add_occupant(person)
+            puts "check in successful"
+        else
+            puts "sorry, room is full"
+        end
+    else
+        puts "sorry, room does not exist"
+    end
+  end
+
+  def has_vacancy?
+    return !@rooms.values.all? {|room| room.full? }
+  end
+
+  def list_rooms
+    @rooms.each do |name, room|
+        puts name + " : " + room.available_space.to_s
+    end
+  end
+
 end
--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,27 @@
 class Room
   
+  attr_reader :capacity, :occupants
+
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def full?
+    return @occupants.length == @capacity
+  end
+
+  def available_space
+    return @capacity - @occupants.length
+  end
+
+  def add_occupant(name)
+    if full?
+      return false
+    else
+      @occupants << name
+      return true
+    end
+  end
+
 end
